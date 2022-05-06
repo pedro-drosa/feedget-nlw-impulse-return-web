43ab2529c9bfff332a8bb8f6f0fa0b7549f98bfc
pedro-drosa <pedro_drosa@hotmail.com>
2022-05-06 11:16:00 -0300

    feat(FeedBackContentStep): creating feedback form
    - created textarea styling
    
    - button to reset message type
    
    - added two external plugins (tailwind.config): tailwindcss/form and tailwind-scrollbar

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { CloseButton } from "../CloseButton";
+
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeebackContentStep";
 
 import bugImageUrl from "../../assets/bug.svg";
 import ideaImageUrl from "../../assets/idea.svg";
@@ -35,16 +36,19 @@ export type FeedBackType = keyof typeof feedbackTypes;
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedBackType | null>(null);
 
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
       {!feedbackType ? (
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
       ) : (
-        <p>Hello</p>
+        <FeedbackContentStep
+          onFeedbackRestartRequested={handleRestartFeedback}
+          feedbackType={feedbackType}
+        />
       )}
 
       <footer className="text-xs text-neutral-400">
